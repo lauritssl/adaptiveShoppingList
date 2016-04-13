3eafd5e5e2e7fdc3ecfaeb3e9ae4855aee6a7ac0
Laurits Langberg <laurits.langberg@gmail.com>
2016-04-13 19:33:52 +0200

    mongo database

---
+++ databaseCalls.js
@@ -0,0 +1,19 @@
+db.recipes.aggregate(
+    // break apart documents to by the products subdocuments
+    { $unwind : "$ingredients" },
+    // look for matches
+    { $match : {'ingredients.name': {$in : ["bacon", "persille"]}}},
+    // search for matches in the sub documents and add productMatch if a match is found
+    { $project : {
+        _id : 1,
+        ingredname : "$ingredients.name"
+    }},
+    { $group : {
+        _id : "$_id",
+        numMatches : { $sum : +1 }
+    }},
+    { $sort : { numMatches : -1 }},
+    { $limit : 1 }
+);
+
+db.getCollection('recipes').find({_id : ObjectId("570e20f91b23d27af8934bc7")});
--- server.js
+++ server.js
@@ -1,34 +1,69 @@
 var restify = require('restify');
+var mongojs = require('mongojs');
+
+
 port = 8080;
 var list = [];
+var recipe = null;
 
 //Creating server using restify
 var server = restify.createServer();
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+// server.use(restify.queryParser());
+// server.use(restify.bodyParser());
+
+var db = mongojs('adaptiveshoppinglist');
+var recipes = db.collection("recipes");
+
+server.get('/list/', function (req, res, next){
+  // res.send('list', list);
+  res.write('Done!');
+  res.end();
+});
+
+server.get('/recipes/:recipeName', function (req, res, next){
+  console.log(req.params.recipeName);
+  res.send(200, recipe);
+
+  res.end();
+});
+
+
+server.post('/list/add/:product', function (req, res, next){
+  product = req.params.product;
+  list.push(product);
+  console.log(list);
+
+  // recipe = recipes.aggregate(
+  //     // break apart documents to by the products subdocuments
+  //     { $unwind : "$ingredients" },
+  //     // look for matches
+  //     { $match : {'ingredients.name': {$in : list}}},
+  //     // search for matches in the sub documents and add productMatch if a match is found
+  //     { $project : {
+  //         _id : 1,
+  //         ingredname : "$ingredients.name"
+  //     }},
+  //     { $group : {
+  //         _id : "$_id",
+  //         numMatches : { $sum : +1 }
+  //     }},
+  //     { $sort : { numMatches : -1 }},
+  //     { $limit : 1 }
+  // );
+  res.end();
+});
+
+server.post('/finish/', function (req, res, next){
+  list = [];
+  console.log('list is finished');
+});
+
 
 //Return default interface
 server.get('/.*/', restify.serveStatic({
   directory: __dirname,
   default: 'index.html'
 }));
-
-server.get('/list', function (req, res, next){
-  res.send('list', list);
-  res.write('Done!');
-  res.end()
-});
-
-server.post('/list/add/:product', function (req, res, next){
-  list.push(req.params.product);
-  console.log(list)
-  res.end()
-});
-
-server.post('/finish', function (req, res, next){
-  list = [];
-  console.log('list is finished');
-});
 
 server.listen(port, function () {
   console.log('%s listening at %s', server.name, port);
