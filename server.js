6e617f1c8af0ac1fb01f91d379934f7e033ce4ca
Laurits Langberg <laurits.langberg@gmail.com>
2016-04-13 11:32:04 +0200

    First working version

--- server.js
+++ server.js
@@ -4,17 +4,29 @@ var list = [];
 
 //Creating server using restify
 var server = restify.createServer();
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
 
 //Return default interface
-server.get('/', restify.serveStatic({
-  directory: './',
+server.get('/.*/', restify.serveStatic({
+  directory: __dirname,
   default: 'index.html'
 }));
 
+server.get('/list', function (req, res, next){
+  res.send('list', list);
+  res.write('Done!');
+  res.end()
+});
+
 server.post('/list/add/:product', function (req, res, next){
-  console.log(request.params.product);
-  list.push(request.params.product);
+  list.push(req.params.product);
   console.log(list)
+  res.end()
+});
+
+server.post('/finish/', function (req, res, next){
+  list = [];
 });
 
 server.listen(port, function () {
