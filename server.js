123d3b3c3ccf7ebb5c5e3c9c46d30f3142130ed1
Laurits Langberg <laurits.langberg@gmail.com>
2016-04-14 10:26:04 +0200

    Search logic + tester page

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongojs = require('mongojs');
 
 port = 8080;
 var list = [];
-var recipe = null;
+var recipe = [];
 
 //Creating server using restify
 var server = restify.createServer();
@@ -20,8 +20,8 @@ server.get('/list/', function (req, res, next){
   res.end();
 });
 
-server.get('/recipes/:recipeName', function (req, res, next){
-  console.log(req.params.recipeName);
+server.get('/recipes/', function (req, res, next){
+  console.log(recipe);
   res.send(200, recipe);
 
   res.end();
@@ -32,24 +32,7 @@ server.post('/list/add/:product', function (req, res, next){
   product = req.params.product;
   list.push(product);
   console.log(list);
-
-  // recipe = recipes.aggregate(
-  //     // break apart documents to by the products subdocuments
-  //     { $unwind : "$ingredients" },
-  //     // look for matches
-  //     { $match : {'ingredients.name': {$in : list}}},
-  //     // search for matches in the sub documents and add productMatch if a match is found
-  //     { $project : {
-  //         _id : 1,
-  //         ingredname : "$ingredients.name"
-  //     }},
-  //     { $group : {
-  //         _id : "$_id",
-  //         numMatches : { $sum : +1 }
-  //     }},
-  //     { $sort : { numMatches : -1 }},
-  //     { $limit : 1 }
-  // );
+  findRecipe(list);
   res.end();
 });
 
@@ -68,3 +51,48 @@ server.get('/.*/', restify.serveStatic({
 server.listen(port, function () {
   console.log('%s listening at %s', server.name, port);
 });
+
+function findRecipe(list){
+  recipe = [];
+
+  searchDoc = null;
+  recipes.aggregate([
+    // break apart documents to by the products subdocuments
+    { $unwind : "$ingredients" },
+    // look for matches
+    { $match : {'ingredients.name': {$in : list}}},
+    // search for matches in the sub documents and add productMatch if a match is found
+    { $project : {
+        _id : 1,
+        ingredname : "$ingredients.name"
+    }},
+    { $group : {
+        _id : "$_id",
+        numMatches : { $sum : +1 }
+    }},
+    { $sort : { numMatches : -1 }},
+    { $limit : 3 }
+  ], function(err, doc){
+    if(doc){
+      // console.log(doc);
+      searchDoc = doc;
+      console.log(searchDoc);
+      recipeList = [];
+      for (var i = 0; i < searchDoc.length; i++) {
+        recipeList.push(mongojs.ObjectId(searchDoc[i]._id));
+      }
+      console.log(recipeList);
+      for (var i = 0; i < recipeList.length; i++) {
+        recipes.findOne({
+          _id: recipeList[i]
+        }, function(err, doc) {
+            if(doc){
+              console.log(doc);
+              recipe.push(doc);
+            }
+        });
+
+      }
+    }
+  });
+}
